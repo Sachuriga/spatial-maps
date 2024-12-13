32f9bfa37f8cd1323bb1c165d2d883089de6dfdd
Sachuriga <sachuriga3@gmail.com>
2024-12-13 09:46:53 +0100

    test_0.2

--- spatial_maps/data.py
+++ spatial_maps/data.py
@@ -1,7 +1,7 @@
 from scipy.ndimage import gaussian_filter1d
 import numpy as np
 
-def pos2speed(t,x,y,filter_speed=True):
+def pos2speed(t,x,y,filter_speed=True,min_speed=0.05):
     delta_X = np.diff(x)
     delta_Y = np.diff(y)
     sampling_intervals = np.diff(t)
@@ -17,11 +17,27 @@ def pos2speed(t,x,y,filter_speed=True):
     delta_S = np.sqrt(delta_X**2 + delta_Y**2)
     speeds = delta_S*samplingrate
 
-    # Compute instantaneous speeds
-    smoothed_speed = gaussian_filter1d(speeds, sigma=sigma)
-    mask = (smoothed_speed>=0.05)
 
     if filter_speed==True:
-        return mask, smoothed_speed
-    else:
-        return smoothed_speed
+        smoothed_speed = gaussian_filter1d(speeds, sigma=sigma)
+        mask = (smoothed_speed>=min_speed)
+        speeds = smoothed_speed[mask]
+
+    xx=x[1:]
+    yy=y[1:]
+    tt=t[1:]
+    x = xx[mask]
+    y= yy[mask]
+    t = tt[mask]
+
+    combined_array = np.column_stack((t, x, y))
+
+    return combined_array, mask, speeds
+
+def speed_filtered_spikes(spikes_time,t,mask):
+    t_ = np.append(t[1:], t[1:][-1] + np.median(np.diff(t[1:])))
+    t_ = np.append(t[1:], t[1:][-1] + np.median(np.diff(t[1:])))
+    spikes_in_bin, _ = np.histogram(spikes_time, t_)
+    spk = spikes_in_bin[mask]
+
+    return spk
